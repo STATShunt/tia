436fd0bb80cab5eb3bc23757253f13624c13b89c
STATShunt <ian@tagroup.co.nz>
2022-02-22 15:55:40 +1100

    update

---
+++ code/MAIN_buds.R
@@ -0,0 +1,37 @@
+cat("\014")  # clean console window
+rm(list=ls(pos=.GlobalEnv), pos=.GlobalEnv) # remove all variables and start again :)
+
+# check: where is your working directory (this is where things will be saved)
+getwd()
+
+############################################################################################
+##  read in data and view
+############################################################################################
+
+# read in raw file, formatted with columns set by a comma
+#xbud<-read.csv(file="data_buds.csv",check.names = FALSE,stringsAsFactors =TRUE) # assumes clean CSV file in working directory
+
+# read in raw file from GITHUB, formatted with columns set by a comma
+xurl<-"https://raw.githubusercontent.com/STATShunt/tia/master/data/cannabis/data_buds.csv"
+xbud<-read.csv(file=xurl,check.names = FALSE,stringsAsFactors =TRUE)
+
+#open data in an RStudioe window (just for looking at ...)
+View(xbud)
+
+############################################################################################
+##  summarise data
+############################################################################################
+
+# make a simple summary function
+fsum_simp<-function(i){
+  out<-c(length(i),mean(i),sd(i))
+  names(out)<-c("n","mean","sd")
+  out
+  }
+
+# summarise
+out_sum<-aggregate(CBDA~treatment,data=xbud,FUN=fsum_simp)
+
+#output to working directory
+write.table(file="summary_stats_buds.CSV",out_sum,sep=",",row.names = FALSE)
+
