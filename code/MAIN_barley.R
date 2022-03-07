e792a6f68ca45df3f0d13781d32f51784b79d8f0
STATShunt <ian@tagroup.co.nz>
2022-03-08 08:49:12 +1100

    update

--- code/MAIN_barley.R
+++ code/MAIN_barley.R
@@ -12,7 +12,7 @@ getwd()
 #x<-read.csv(file="data_barley.csv",check.names = FALSE,stringsAsFactors =TRUE) # assumes clean CSV file in working directory
 
 # read in raw file from GITHUB, formatted with columns set by a comma
-xurl<-"https://raw.githubusercontent.com/STATShunt/tia/master/data/barley/data_barley.CSV"
+xurl<-"https://raw.githubusercontent.com/STATShunt/tia/master/data/barley/data_barley.csv"
 x<-read.csv(file=xurl,check.names = FALSE,stringsAsFactors =TRUE)
 
 #open data in an RStudio window (just for looking at ...)
