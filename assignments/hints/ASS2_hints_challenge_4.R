952fe45adcc1d35b800a682cd465c609638aa127
STATShunt <ian@tagroup.co.nz>
2022-05-02 16:23:30 +1000

    update

---
+++ assignments/hints/ASS2_hints_challenge_4.R
@@ -0,0 +1,15 @@
+# the following code is indicative of what you need to do
+# do not "cut and paste", rather, "run the code and think".
+# then alter code carefully to answer the challenge's questions
+
+###################################################
+###  CHALLENGE 4
+###################################################
+
+# q1
+# hint just download the Spiegelhalter code and run.
+# save it as separate script, not in your answer
+
+# q2 
+# run the code for different nsamp<-15 (this is the theoretical sample size being repeatedly drawn)
+# for different scenarios check the histogram and qqplots
--- assignments/hints/ASS2_hints_challenge_5.R
+++ assignments/hints/ASS2_hints_challenge_5.R
@@ -1,17 +1,21 @@
+# the following code is indicative of what you need to do
+# do not "cut and paste", rather, "run the code and think".
+# then alter code carefully to answer the challenge's questions
+
 ###################################################
 ###  CHALLENGE 5
 ###################################################
 x<-rnorm(10,99,20)  # not the x in the challenge!
 
 ## main idea is to resample from the rows (1:nrow(x)) of x many times ---> then use these index numbers to point to the data and make new bootstrap-samples.
-nboot<-1000 # number of bootstrap samples to take
+nboot<-5 # number of bootstrap samples to take (you should do 10,000)
 nsamp<-length(x) # make sure bootstrap sample is size as original
 
 index_boot1<-sample(1:length(x),nsamp,replace = TRUE)
 x; index_boot1
 
 # question 1  
-mean(x[index_boot1])
+mean(x[index_boot1]) # mean of one bootstrap sample!
 
 # question 2
 # hint: use lapply to do things over and over
@@ -23,7 +27,7 @@ boot_mean_vector<-unlist(boot_mean) # trick to turn list into a vector
 hist(boot_mean_vector)
 
 # question 3
-mean(boot_mean_vector) #hmm?  what is this???
+mean(boot_mean_vector) #hmm?  what is this converging to as the number of bootstrap samples increases???
 
 # question 4
 # hint: choose a reasonable range and report it ...
