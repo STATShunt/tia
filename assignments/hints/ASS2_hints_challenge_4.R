1429608ed5ff7d730f8ca9c82a4437591d7a8535
STATShunt <ian@tagroup.co.nz>
2022-05-02 16:45:07 +1000

    update

--- assignments/hints/ASS2_hints_challenge_4.R
+++ assignments/hints/ASS2_hints_challenge_4.R
@@ -8,8 +8,8 @@
 
 # q1
 # hint just download the Spiegelhalter code and run.
-# save it as separate script, not in your answer script
+# save it as a separate script, not in your answer script
 
 # q2 
-# run the code for different nsamp<-15 (this is the theoretical sample size being repeatedly drawn)
+# run the code for different "nsamp<-15" (this is the theoretical sample size being repeatedly drawn)
 # for different scenarios check the histogram and qqplots
